c24c42c0a29b66d6a5052db471bab4bba75c3b83
Serh Jeff <jeffserh7@gmail.com>
2026-03-08 18:11:37 +0100

    v2.0

--- apps/games/dashboard_urls.py
+++ apps/games/dashboard_urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views
+from . import views_dashboard
 
 app_name = 'games_dashboard'
 
 urlpatterns = [
-    path('', views.my_games, name='my_games'),
-    path('publish/', views.publish_game, name='publish'),
+    path('', views_dashboard.my_games, name='my_games'),
 ]
